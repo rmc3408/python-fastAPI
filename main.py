76a549101ad374bf5695b2f9f9ad3f6c0320378d
Raphael <rmc3408@protonmail.com>
2023-06-14 15:24:12 -0400

    install VENV

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello world')
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def get_root():
+    return {"msg": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "query": q}
